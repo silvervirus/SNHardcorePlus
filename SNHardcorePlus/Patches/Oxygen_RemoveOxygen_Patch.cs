36d3dd418605e056484f8fa81f58380da27bdf16
MrPurple6411 <mrpurple6411@gmail.com>
2020-03-02 22:50:38 -0700

    Fixed Craftdata and AirBladder
    Now Requires SMLHelper

--- SNHardcorePlus/Patches/Oxygen_RemoveOxygen_Patch.cs
+++ SNHardcorePlus/Patches/Oxygen_RemoveOxygen_Patch.cs
@@ -4,20 +4,12 @@ using UnityEngine;
 namespace SNHardcorePlus.Patches
 {
     [HarmonyPatch(typeof(Oxygen))]
-    [HarmonyPatch("RemoveOxygen")]
+    [HarmonyPatch(nameof(Oxygen.RemoveOxygen))]
     class Oxygen_RemoveOxygen_Patch
     {
-        public static bool Prefix(Oxygen __instance, float __result, ref float amount)
+        public static void Prefix(ref float amount)
         {
-            var cleanAmount = Mathf.Min(amount, __instance.oxygenAvailable);
-
-            float num = Mathf.Min(amount * HCPSettings.Instance.OxygenDrainMultiplier, __instance.oxygenAvailable);
-
-            __instance.oxygenAvailable = Mathf.Max(0f, __instance.oxygenAvailable - num);
-
-            __result = cleanAmount;
-
-            return false;
+            amount = amount * HCPSettings.Instance.OxygenDrainMultiplier;
         }
     }
 }
